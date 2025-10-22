108bc7cc38ce856a52b6dc7452f64fc0f64813fe
BKHornYT <boysgunsmoke@gmail.com>
2025-10-22 08:51:36 +0200

    css update

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const startChatButton = document.getElementById('start-chat-btn');
 startChatButton.addEventListener('click', () => {
     window.location.href = 'chat.html';
 }); // BK Skrev denne koden for å få knappen til å fungere
+
