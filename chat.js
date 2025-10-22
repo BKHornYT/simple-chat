8f74f4d5af5ac0505c9e0acd4696424eba63f980
BKHornYT <boysgunsmoke@gmail.com>
2025-10-22 09:04:16 +0200

    test

---
+++ chat.js
@@ -0,0 +1,5 @@
+// Select elements
+const sendBtn = document.getElementById("send-btn");
+const userInput = document.getElementById("user-input");
+const messagesDiv = document.getElementById("messages");
+
